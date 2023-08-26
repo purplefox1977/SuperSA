d4058c2e4b33c4d3546afe558e13c8eced41e0a2
purplefox1977 <141675561+purplefox1977@users.noreply.github.com>
2023-08-26 01:37:45 -0300

    Busqueda por precio

--- SuperSA/src/main/java/com/mycompany/supersa/BusquedaPorPrecioView.java
+++ SuperSA/src/main/java/com/mycompany/supersa/BusquedaPorPrecioView.java
@@ -3,18 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
 package com.mycompany.supersa;
-
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Loboplateado77
  */
 public class BusquedaPorPrecioView extends javax.swing.JInternalFrame {
-
+private DefaultTableModel modelo = new DefaultTableModel();
     /**
      * Creates new form BusquedaPorPrecioView
      */
     public BusquedaPorPrecioView() {
         initComponents();
+        agregarCabecera();
     }
 
     /**
@@ -26,23 +27,150 @@ public class BusquedaPorPrecioView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTmenorPrecio = new javax.swing.JTextField();
+        jTmayorPrecio = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTProductos = new javax.swing.JTable();
+
         setClosable(true);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 153, 255));
+        jLabel1.setText("Busqueda por Precio");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 153, 153));
+        jLabel2.setText("Entre $");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 153, 153));
+        jLabel3.setText("y");
+
+        jTmenorPrecio.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTmenorPrecioKeyReleased(evt);
+            }
+        });
+
+        jTmayorPrecio.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTmayorPrecioKeyReleased(evt);
+            }
+        });
+
+        jTProductos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTProductos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jTmenorPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jTmayorPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 381, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(68, 68, 68))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTmenorPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jTmayorPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jTmenorPrecioKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTmenorPrecioKeyReleased
+        // TODO add your handling code here:
+         buscarFilas();
+       //entonces me interesa a ese producto agregarlo a la lista
+       //agrega la fila como un objeto 
+       
+        for (Producto prod : Menu.listaProductos ) {
+            if(prod.getPrecio()> Integer.parseInt(jTmenorPrecio.getText())&&prod.getPrecio()< Integer.parseInt(jTmayorPrecio.getText())  ){
+                modelo.addRow(new Object[]{ prod.getCodigo(),
+                                            prod.getDescripcion(), 
+                                            prod.getPrecio(),
+                                            prod.getStock()
+                });                       
+            }
+        }   
+    }//GEN-LAST:event_jTmenorPrecioKeyReleased
+
+    private void jTmayorPrecioKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTmayorPrecioKeyReleased
+        // TODO add your handling code here:
+             buscarFilas();
+       //entonces me interesa a ese producto agregarlo a la lista
+       //agrega la fila como un objeto 
+       
+        for (Producto prod : Menu.listaProductos ) {
+            if(prod.getPrecio()< Integer.parseInt(jTmayorPrecio.getText()) &&prod.getPrecio()> Integer.parseInt(jTmenorPrecio.getText()) ){
+                modelo.addRow(new Object[]{ prod.getCodigo(),
+                                            prod.getDescripcion(), 
+                                            prod.getPrecio(),
+                                            prod.getStock()
+                });                       
+            }
+        }
+    }//GEN-LAST:event_jTmayorPrecioKeyReleased
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTProductos;
+    private javax.swing.JTextField jTmayorPrecio;
+    private javax.swing.JTextField jTmenorPrecio;
     // End of variables declaration//GEN-END:variables
+     private void agregarCabecera(){
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Descripcion");
+        modelo.addColumn("Precio");
+        modelo.addColumn("Stock");
+        
+        jTProductos.setModel(modelo);        
+    }
+    private void buscarFilas(){
+        
+        int filas = jTProductos.getRowCount()-1; 
+            for(int f = filas;f>=0;f--){
+                 modelo.removeRow(f);
+            }   
+    }
+
 }
